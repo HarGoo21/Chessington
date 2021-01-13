1338ea6048b3f2ea443579fad3145b7132d5306f
Harry Goodburn <Harry.Goodburn@softwire.com>
2021-01-13 11:09:40 +0000

    added initial queen movement

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,27 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            var availableMoves = new List<Square>();
+
+            for (var i = 0; i < GameSettings.BoardSize; i++)
+            {
+                availableMoves.Add(Square.At(i,currentSquare.Col));
+                availableMoves.Add(Square.At(currentSquare.Row,i));
+            }
+
+            for (var i = 0; i < GameSettings.BoardSize; i++)
+            {
+                availableMoves.Add(Square.At(i, i));
+            }
+
+            for (var i = 1; i < GameSettings.BoardSize; i++)
+            {
+                availableMoves.Add(Square.At(i, 8 - i));
+            }
+
+            availableMoves.RemoveAll(s => s == currentSquare);
+            return availableMoves;
         }
     }
 }
