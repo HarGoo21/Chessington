d64b9612b4a9b726cbba61bc96b392aa64001369
Harry Goodburn <Harry.Goodburn@softwire.com>
2021-01-13 10:41:34 +0000

    Changed pawns to be able to move two squares if unmoved and only one if moved (row index for white squares appears to be wrong, may need fixing)

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,31 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            
+            var currentSquare = board.FindPiece(this);
+            if (this.Player == Player.White)
+            {
+                if (currentSquare.Row == 7)
+                {
+                    return new List<Square> {Square.At(currentSquare.Row - 2, currentSquare.Col), Square.At(currentSquare.Row - 1, currentSquare.Col)};
+                }
+                else
+                {
+                    return new List<Square> {Square.At(currentSquare.Row - 1, currentSquare.Col)};
+                }
+            }
+            else
+            {
+                if (currentSquare.Row == 1)
+                {
+                    return new List<Square> {Square.At(currentSquare.Row + 2, currentSquare.Col), Square.At(currentSquare.Row + 1, currentSquare.Col)};
+                }
+                else
+                {
+                    return new List<Square> {Square.At(currentSquare.Row + 1, currentSquare.Col)};
+                }
+                    
+                
+            }
             return new List<Square> {Square.At(6, 0), Square.At(2, 0)};
         }
     }
