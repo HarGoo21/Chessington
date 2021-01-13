f278ca077ba202a15ebec5c9a44e32832e1c0e72
Harry Goodburn <Harry.Goodburn@softwire.com>
2021-01-13 11:15:08 +0000

    added initial knight movement

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,8 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            return new List<Square> {Square.At(3,2), Square.At(3,6), Square.At(2,3), Square.At(2,5),
+                Square.At(5,2), Square.At(5, 6), Square.At(6,3), Square.At(6,5)};
         }
     }
 }
